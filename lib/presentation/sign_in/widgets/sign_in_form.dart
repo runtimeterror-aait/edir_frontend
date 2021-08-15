6baa82b8df51fd8db225d3cdda5c3039824dbcdc
Liyu M <liyuumk@gmail.com>
2021-08-15 16:03:55 +0300

    Finished sign in page ui.

--- lib/presentation/sign_in/widgets/sign_in_form.dart
+++ lib/presentation/sign_in/widgets/sign_in_form.dart
@@ -7,55 +7,87 @@ class SignInForm extends StatelessWidget {
   Widget build(BuildContext context) {
     final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
       onPrimary: Colors.black87,
-      primary: Colors.amber[300],
-      minimumSize: Size(88, 36),
-      padding: EdgeInsets.symmetric(horizontal: 20),
+      primary: Colors.amber,
+      minimumSize: Size(50, 45),
       shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(2)),
+        borderRadius: BorderRadius.all(Radius.circular(5)),
       ),
     );
     return Form(
         child: ListView(
       children: [
+        SizedBox(height: 30),
         Center(
-          child: Text(
-            'Edir',
-            style: TextStyle(fontSize: 30),
+            child: CircleAvatar(
+          radius: 60,
+          backgroundImage: AssetImage('assets/images/edir_logo.png'),
+        )),
+        SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+          child: TextField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.email),
+              labelText: 'Email',
+            ),
+            autocorrect: false,
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+          child: TextField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.lock),
+              labelText: 'Password',
+            ),
+            autocorrect: false,
+            obscureText: true,
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(30, 0, 30, 10),
+          child: ElevatedButton(
+            style: raisedButtonStyle,
+            onPressed: () {},
+            child: const Text('Sign in'),
           ),
         ),
         SizedBox(
           height: 20,
         ),
-        TextField(
-          decoration: InputDecoration(
-            prefixIcon: Icon(Icons.email),
-            labelText: 'Email',
-          ),
-          autocorrect: false,
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        TextField(
-          decoration: InputDecoration(
-            prefixIcon: Icon(Icons.lock),
-            labelText: 'Password',
-          ),
-          autocorrect: false,
-          obscureText: true,
-        ),
-        const SizedBox(
-          height: 10,
-        ),
         Row(
-          children: <Widget>[
-            Expanded(
-              child: ElevatedButton(
-                style: raisedButtonStyle,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            TextButton.icon(
                 onPressed: () {},
-                child: const Text('Sign in'),
-              ),
-            ),
+                icon: Icon(
+                  Icons.email,
+                  color: Colors.grey[200],
+                ),
+                label: const Text(
+                  'SIGN IN WITH GOOGLE',
+                  style: TextStyle(color: Colors.grey),
+                )),
+            TextButton.icon(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.person_add,
+                  color: Colors.grey[200],
+                ),
+                label: const Text(
+                  'REGISTER',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                )),
           ],
         ),
       ],
