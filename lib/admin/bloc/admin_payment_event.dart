0acc25f34cb2d74d68b814757ec0e1c150bbc867
Liyu M <liyuumk@gmail.com>
2021-09-10 12:11:09 +0300

    payment bloc and repository

--- lib/admin/bloc/admin_payment_event.dart
+++ lib/admin/bloc/admin_payment_event.dart
@@ -6,3 +6,25 @@ abstract class AdminPaymentEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class GetAllPaymentsEvent extends AdminPaymentEvent {
+  final int memberId;
+  GetAllPaymentsEvent({required this.memberId});
+  @override
+  List<Object> get props => [memberId];
+}
+
+class AddPaymentEvent extends AdminPaymentEvent {
+  final int memberId;
+  final Payment payment;
+  AddPaymentEvent({required this.memberId, required this.payment});
+  @override
+  List<Object> get props => [memberId, payment];
+}
+
+class RemovePaymentEvent extends AdminPaymentEvent {
+  final int memberId;
+  RemovePaymentEvent({required this.memberId});
+  @override
+  List<Object> get props => [memberId];
+}
